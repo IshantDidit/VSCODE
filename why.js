46c18335c622cb0baddb264c1529d8378e44e467
IshantDidit <Rinnesonogami@proton.me>
2026-02-11 10:23:17 +0545

    who knows

--- why.js
+++ why.js
@@ -1,21 +1,14 @@
-/*
+let students = [
+    {
+        roll:1,
+        courses:["web", "mobile"]
+    },
+    {
+        roll:1,
+        courses:["ML", "mobile"]
+    }
 
-let a = '" "'
 
-let color = {
-    name: "red",
-    hexValue: "#FF0000",
-    rgbValue: "rgba(255,0,0)"
-}
-
-console.log(`hex value of color ${color.name} is ${color.hexValue}`)
-*/
-let person = {
-    first_name : "john",
-    last_name : "dice",
-    age :"68",
-    webesite: "john@dice.com"
-
-}
-
-console.log("my name is" + first_name)
+]
+students[1].courses[1]="Artificial intelligence"
+console.log(students)
